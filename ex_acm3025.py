b1a0822042aea4f4f504aac4460ffdc1bd530d5b
Jhy <jhyinchina@gmail.com>
2019-03-31 22:45:37 +0800

    Update ex_acm3025.py

--- ex_acm3025.py
+++ ex_acm3025.py
@@ -8,7 +8,7 @@ from utils import process
 # 禁用gpu
 import os
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"#""0,1,2,3"
 
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
@@ -18,14 +18,14 @@ checkpt_file = 'pre_trained/{}/{}_allMP.ckpt'.format(dataset, dataset)
 
 # training params
 batch_size = 1
-nb_epochs = 300#20
+nb_epochs = 600
 patience = 100
 lr = 0.004  # learning rate
 l2_coef = 0.001  # weight decay
 att_size = 128
 # numbers of hidden units per each attention head in each layer
 hid_units = [8]
-n_heads = [1, 1]  # additional entry for the output layer
+n_heads = [8, 1]  # additional entry for the output layer
 residual = False
 nonlinearity = tf.nn.elu
 
@@ -59,7 +59,7 @@ def load_data_dblp(path='/home/jhy/allGAT/acm_hetesim/ACM3025.mat'):
     data = sio.loadmat(path)
     truelabels, truefeatures = data['label'], data['feature'].astype(float)
     N = truefeatures.shape[0]
-    rownetworks = [data['PAP'] - np.eye(N), data['PLP'] - np.eye(N)]  # , data['PTP'] - np.eye(N)]
+    rownetworks = [data['PAP'] - np.eye(N), data['PLP'] - np.eye(N)] 
 
     y = truelabels
     train_idx = data['train_idx']
@@ -89,6 +89,7 @@ def load_data_dblp(path='/home/jhy/allGAT/acm_hetesim/ACM3025.mat'):
 
 
 adj_list, features, y_train, y_val, y_test, train_mask, val_mask, test_mask = load_data_dblp()
+
 
 
 nb_nodes = features.shape[0]
@@ -129,8 +130,7 @@ with tf.Graph().as_default():
                                                                   hid_units=hid_units, n_heads=n_heads,
                                                                   residual=residual, activation=nonlinearity,
                                                                   mp_att_size=att_size,  # 0805最后的挣扎
-                                                                  return_coef=True
-                                                                  )
+                                                                  return_coef=True)
 
     # cal masked_loss
     log_resh = tf.reshape(logits, [-1, nb_classes])
@@ -158,109 +158,42 @@ with tf.Graph().as_default():
         val_loss_avg = 0
         val_acc_avg = 0
 
-        for epoch in range(nb_epochs):
-            tr_step = 0
-            tr_size = features.shape[0]
-            # ================   training    ============
-            while tr_step * batch_size < tr_size:
-                fd1 = {ftr_in: features[tr_step * batch_size:(tr_step + 1) * batch_size]}
-                fd2 = {i: d[tr_step * batch_size:(tr_step + 1) * batch_size]
-                       for i, d in zip(bias_in_list, biases_list)}
-                fd3 = {lbl_in: y_train[tr_step * batch_size:(tr_step + 1) * batch_size],
-                       msk_in: train_mask[tr_step * batch_size:(tr_step + 1) * batch_size],
-                       is_train: True,
-                       attn_drop: 0.6,
-                       ffd_drop: 0.6}
-                fd = fd1
-                fd.update(fd2)
-                fd.update(fd3)
-                _, loss_value_tr, acc_tr, att_val_train = sess.run([train_op, loss, accuracy, att_val],
-                                                                   feed_dict=fd)
-                train_loss_avg += loss_value_tr
-                train_acc_avg += acc_tr
-                tr_step += 1
-
-            vl_step = 0
-            vl_size = features.shape[0]
-            # =============   val       =================
-            while vl_step * batch_size < vl_size:
-                fd1 = {ftr_in: features[vl_step * batch_size:(vl_step + 1) * batch_size]}
-                fd2 = {i: d[vl_step * batch_size:(vl_step + 1) * batch_size]
-                       for i, d in zip(bias_in_list, biases_list)}
-                fd3 = {lbl_in: y_train[vl_step * batch_size:(vl_step + 1) * batch_size],
-                       msk_in: train_mask[vl_step * batch_size:(vl_step + 1) * batch_size],
-                       is_train: False,
-                       attn_drop: 0.0,
-                       ffd_drop: 0.0}
-                fd = fd1
-                fd.update(fd2)
-                fd.update(fd3)
-                loss_value_vl, acc_vl = sess.run([loss, accuracy],
-                                                 feed_dict=fd)
-                val_loss_avg += loss_value_vl
-                val_acc_avg += acc_vl
-                vl_step += 1
-            # import pdb; pdb.set_trace()
-            print('Epoch: {}, att_val: {}'.format(epoch, np.mean(att_val_train, axis=0)))
-            print('Training: loss = %.5f, acc = %.5f | Val: loss = %.5f, acc = %.5f' %
-                  (train_loss_avg / tr_step, train_acc_avg / tr_step,
-                   val_loss_avg / vl_step, val_acc_avg / vl_step))
-
-            if val_acc_avg / vl_step >= vacc_mx or val_loss_avg / vl_step <= vlss_mn:
-                if val_acc_avg / vl_step >= vacc_mx and val_loss_avg / vl_step <= vlss_mn:
-                    vacc_early_model = val_acc_avg / vl_step
-                    vlss_early_model = val_loss_avg / vl_step
-                    saver.save(sess, checkpt_file)
-                vacc_mx = np.max((val_acc_avg / vl_step, vacc_mx))
-                vlss_mn = np.min((val_loss_avg / vl_step, vlss_mn))
-                curr_step = 0
-            else:
-                curr_step += 1
-                if curr_step == patience:
-                    print('Early stop! Min loss: ', vlss_mn,
-                          ', Max accuracy: ', vacc_mx)
-                    print('Early stop model validation loss: ',
-                          vlss_early_model, ', accuracy: ', vacc_early_model)
-                    break
-
-            train_loss_avg = 0
-            train_acc_avg = 0
-            val_loss_avg = 0
-            val_acc_avg = 0
-         
-        sess.close()
-        
+        saver.restore(sess, checkpt_file)
+        print('load model from : {}'.format(checkpt_file))
         ts_size = features.shape[0]
         ts_step = 0
         ts_loss = 0.0
         ts_acc = 0.0
 
         while ts_step * batch_size < ts_size:
-            loss_value_ts, acc_ts, batch_final_embedding = sess.run([loss, accuracy, final_embedding],
-                                                                    feed_dict={
-                                                                        ftr_in: features[
-                                                                                ts_step * batch_size:(
-                                                                                                             ts_step + 1) * batch_size],
-                                                                        bias_in: biases[
-                                                                                 ts_step * batch_size:(
-                                                                                                              ts_step + 1) * batch_size],
-                                                                        lbl_in: y_test[
-                                                                                ts_step * batch_size:(
-                                                                                                             ts_step + 1) * batch_size],
-                                                                        msk_in: test_mask[
-                                                                                ts_step * batch_size:(
-                                                                                                             ts_step + 1) * batch_size],
-                                                                        is_train: False,
-                                                                        attn_drop: 0.0, ffd_drop: 0.0})
+            fd1 = {ftr_in: features[ts_step * batch_size:(ts_step + 1) * batch_size]}
+            fd2 = {i: d[ts_step * batch_size:(ts_step + 1) * batch_size]
+                   for i, d in zip(bias_in_list, biases_list)}
+            fd3 = {lbl_in: y_train[ts_step * batch_size:(ts_step + 1) * batch_size],
+                   msk_in: train_mask[ts_step * batch_size:(ts_step + 1) * batch_size],
+                   is_train: False,
+                   attn_drop: 0.0,
+                   ffd_drop: 0.0}
+            fd = fd1
+            fd.update(fd2)
+            fd.update(fd3)
+            loss_value_ts, acc_ts, jhy_final_embedding, jhy_coef_list = sess.run(
+                [loss, accuracy, final_embedding, coef_list],
+                feed_dict=fd)
             ts_loss += loss_value_ts
             ts_acc += acc_ts
             ts_step += 1
 
-        print('Test loss:', ts_loss / ts_step,
+        print('Test l1oss:', ts_loss / ts_step,
               '; Test accuracy:', ts_acc / ts_step)
-        print('start knn, kmean...')
 
-        xx = batch_final_embedding[test_mask]
+        sess.close()
+
+        print('start knn, kmean.....')
+        xx = np.expand_dims(jhy_final_embedding, axis=0)[test_mask]
         yy = y_test[test_mask]
-        print("embedding: {}, label: {}".format(xx.shape, yy.shape))
+        from jhyexps import my_KNN, my_Kmeans, my_TSNE, my_Linear
+
+        my_KNN(xx, yy)
+        my_Kmeans(xx, yy)
 
